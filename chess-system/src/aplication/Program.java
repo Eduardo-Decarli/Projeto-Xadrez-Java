9febaf215cc29c7f84fddfb8eb41602e7d6e9354
Eduardo Decarli <decarliedu@gmail.com>
2023-12-19 11:55:01 -0300

    Primeira classe: Posicao

--- chess-system/src/aplication/Program.java
+++ chess-system/src/aplication/Program.java
@@ -1,12 +1,14 @@
 package aplication;
 
+import boardgame.Position;
+
 public class Program {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Position posicao = new Position(3, 5);
         
-
+        System.out.println(posicao);
     }
     
 }
